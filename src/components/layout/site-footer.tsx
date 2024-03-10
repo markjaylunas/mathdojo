e1804ff499be83b4d53412da358463f9f035f90e
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-03-10 11:54:44 +0800

    center footer

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -3,7 +3,7 @@ import { siteConfig } from "@/lib/config";
 export function SiteFooter() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built by{" "}
           <a
