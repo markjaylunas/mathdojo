ee5572915edb307cc20e69cec0fd425829f2563c
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-03-16 15:13:20 +0800

    Add auth callbacks

--- data/user.ts
+++ data/user.ts
@@ -9,3 +9,13 @@ export const getUserByEmail = async (params: { email: string }) => {
   });
   return user;
 };
+
+export const getUserById = async (params: { id: string }) => {
+  const { id } = params;
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  return user;
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,19 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@lib/prisma";
 import authConfig from "./auth.config";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +21,28 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById({ id: token.sub });
+
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
