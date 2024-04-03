6259e28d427b4d5c0440c16a17f7faa559a0564b
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-04-03 13:01:41 +0800

    Add max width

--- src/components/game/layout/GameLayout.tsx
+++ src/components/game/layout/GameLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 const GameLayout = ({ children }: Props) => {
   return (
-    <Card className="sm:min-h-[calc(80vh-70px) flex min-h-[calc(85vh)] flex-col p-3">
+    <Card className="sm:min-h-[calc(80vh-70px) mx-auto flex min-h-[calc(85vh)] max-w-[500px] flex-col p-3">
       {children}
     </Card>
   );
