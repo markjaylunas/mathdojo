e3615b8c20e4c22145b35d5dcace36dde0bf74c8
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-04-05 15:53:14 +0800

    Fix game layout

--- src/components/game/layout/GameLayout.tsx
+++ src/components/game/layout/GameLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 const GameLayout = ({ children }: Props) => {
   return (
-    <Card className="sm:min-h-[calc(80vh-70px) mx-auto flex min-h-[calc(85vh)] max-w-[500px] flex-col justify-between p-3">
+    <Card className="sm:min-h-[calc(80vh-70px) sm:border-1 mx-auto flex min-h-[calc(85vh)] max-w-[500px] flex-col justify-between border-hidden p-0 sm:border-solid sm:p-3">
       {children}
     </Card>
   );
