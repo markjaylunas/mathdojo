307918273c31cad3ceb959557861f6db23d6301f
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2024-04-21 00:47:47 +0800

    Add row formats

--- src/components/leaderboard/highscore/HighScoreList.tsx
+++ src/components/leaderboard/highscore/HighScoreList.tsx
@@ -8,11 +8,16 @@ import {
   TableHeader,
   TableRow,
 } from "@components/ui/table";
+import UserProfileSection from "../../user/UserProfileSection";
+import { formatNumber } from "@/src/lib/game";
+import { Icons } from "../../ui/icons";
+import { cn } from "@/src/lib/utils";
 
 type Props = {
   highScoreList: HighScore[];
 };
 const HighScoreList = ({ highScoreList }: Props) => {
+  const topThree = [0, 1, 2];
   return (
     <Table>
       <TableCaption>A list of high scores.</TableCaption>
@@ -26,9 +31,37 @@ const HighScoreList = ({ highScoreList }: Props) => {
       <TableBody>
         {highScoreList.map((highScore, highScoreIndex) => (
           <TableRow key={highScore.user.id}>
-            <TableCell className="font-medium">{highScoreIndex + 1}</TableCell>
-            <TableCell>{highScore.user.username}</TableCell>
-            <TableCell className="text-right">{highScore.score}</TableCell>
+            <TableCell>
+              <div className="relative w-10">
+                {topThree.includes(highScoreIndex) && (
+                  <Icons.medal
+                    className={cn(
+                      "size-10 ",
+                      highScoreIndex === 0 && "text-gold-500 ",
+                      highScoreIndex === 1 && "text-silver-500",
+                      highScoreIndex === 2 && "text-bronze-500"
+                    )}
+                  />
+                )}
+                <span
+                  className={cn(
+                    "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-xl font-medium",
+                    topThree.includes(highScoreIndex) && "font-extrabold",
+                    highScoreIndex === 0 && "text-gold-900 ",
+                    highScoreIndex === 1 && "text-silver-900 ",
+                    highScoreIndex === 2 && "text-bronze-900 "
+                  )}
+                >
+                  {highScoreIndex + 1}
+                </span>
+              </div>
+            </TableCell>
+            <TableCell>
+              <UserProfileSection user={highScore.user} />
+            </TableCell>
+            <TableCell className="text-right text-lg">
+              {formatNumber(highScore.score)}
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
