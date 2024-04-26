f2c7e1f3561e242811318d93320e0eae827769cd
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-04-26 13:37:41 +0800

    add reset to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,9 +4,21 @@ import BackButton from "@components/ui/back-button";
 import { Button } from "@components/ui/button";
 import { IconArrowBack, IconHome } from "@tabler/icons-react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { DEFAULT_HOME_PATH } from "../lib/routes";
 
-export default async function ErrorPage() {
+export default async function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
   return (
     <div className="container mx-auto flex max-w-lg flex-col items-center justify-center gap-6 p-8">
       <h2 className="mt-10 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -18,9 +30,7 @@ export default async function ErrorPage() {
       </p>
 
       <div className="flex justify-center gap-4 ">
-        <BackButton className="flex gap-1">
-          <IconArrowBack height={20} /> Get back
-        </BackButton>
+        <Button onClick={reset}>Try again</Button>
         <Link href={DEFAULT_HOME_PATH}>
           <Button className="flex gap-1">
             <IconHome size={18} /> Get to home page
